927fc187a5dfbda7b140aeeb515ca56f8e21eb62
Alex Chesnay <me@alex-chesnay.fr>
2021-01-22 23:29:04 +0100

    test(user): add functional tests
    Add CRUD functional tests that are similar to those for the Customer.

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -2,12 +2,16 @@
 
 namespace App\Tests\Entity;
 
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\User;
 use App\Tests\AssertTrait;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class UserTest extends KernelTestCase
+class UserTest extends ApiTestCase
 {
+    use FixturesTrait;
     use AssertTrait;
 
     /**
@@ -22,6 +26,85 @@ class UserTest extends KernelTestCase
             ->setLastname("LastName")
             ->setEmail("valid@email.fr")
             ->setPassword("demo1234");
+    }
+
+    /**
+     * Update another User than the one logged in
+     */
+    public function testUpdateAnotherUser(): void
+    {
+        $authToken = $this->getAuthToken();
+
+        static::createClient()->request(Request::METHOD_PUT, "/api/users/7", ["auth_bearer" => $authToken, "json" => [
+            "email" => "another_user_email@localhost.dev"
+        ]]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Update a User
+     */
+    public function testUpdateUser(): void
+    {
+        $authToken = $this->getAuthToken();
+
+        $response = static::createClient()->request(Request::METHOD_PUT, "/api/users/1", ["auth_bearer" => $authToken, "json" => [
+            "firstname" => "NewFirstname"
+        ]]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertJsonContains([
+            "@context" => "/api/contexts/User",
+            "@type" => "User",
+            "firstname" => "NewFirstname"
+        ]);
+        $this->assertRegExp('~^/api/users/\d+$~', $response->toArray()['@id']);
+        $this->assertMatchesResourceItemJsonSchema(User::class);
+    }
+
+    /**
+     * Update a User without being logged
+     */
+    public function testUpdateUserWithoutAuthorization(): void
+    {
+        static::createClient()->request(Request::METHOD_PUT, "/api/users/1", ["json" => [
+            "firstname" => "NewFirstname"
+        ]]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Delete another User than the one logged in
+     */
+    public function testDeleteAnotherUser(): void
+    {
+        $authToken = $this->getAuthToken();
+
+        static::createClient()->request(Request::METHOD_DELETE, "/api/users/8", ["auth_bearer" => $authToken]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Delete a User
+     */
+    public function testDeleteUser(): void
+    {
+        $authToken = $this->getAuthToken();
+        static::createClient()->request(Request::METHOD_DELETE, "/api/users/1", ["auth_bearer" => $authToken]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NO_CONTENT);
+        $this->assertNull(static::$container->get('doctrine')->getRepository(User::class)->findOneBy(['email' => 'testUser@localhost.dev']));
+    }
+
+    /**
+     * Delete a User without being logged
+     */
+    public function testDeleteUserWithoutAuthorization(): void
+    {
+        static::createClient()->request(Request::METHOD_DELETE, "/api/users/1");
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
     }
 
     public function testFirstnameTooShort(): void

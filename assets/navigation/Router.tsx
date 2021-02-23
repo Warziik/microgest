e4d70c3396aaf7eded1315d340eb3eac28db8128
Alex <me@alex-chesnay.fr>
2021-02-23 16:39:42 +0100

    build(ui): add typescript language

--- assets/navigation/Router.tsx
+++ assets/navigation/Router.tsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Home from '../pages/Home';
 
-const Router = () => {
+const Router: React.FC = () => {
     return <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home} />

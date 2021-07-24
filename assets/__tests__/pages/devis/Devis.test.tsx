2a15e37a63c6e2f2df2238d161b60df5fd5d1d70
Alex Chesnay <me@alex-chesnay.fr>
2021-07-24 19:29:09 +0200

    test: add simple test for devis page

---
+++ assets/__tests__/pages/devis/Devis.test.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import fetchMock from "jest-fetch-mock";
+import { MemoryRouter } from "react-router";
+import "../../../config/config";
+import { act } from "react-dom/test-utils";
+import { Devis } from "../../../pages/devis/Devis";
+
+fetchMock.enableMocks();
+fetchMock.mockResponse(
+  JSON.stringify({
+    "hydra:member": [
+      {
+        id: 1,
+        chrono: "D-2021-000001",
+        status: "NEW",
+        customer: {
+          id: 1,
+          firstname: "Foo",
+          lastname: "Bar",
+        },
+        sentAt: "2021-03-26T11:48:18+00:00",
+        paidAt: null,
+        paymentDeadline: "2021-03-26T11:48:18+00:00",
+        validityDate: "2021-03-26T11:48:18+00:00",
+        workStartDate: "2021-03-26T11:48:18+00:00",
+        workDuration: "2 weeks",
+        paymentDelayRate: 0,
+        services: [{ name: "", quantity: 1, unitPrice: 0 }],
+        tvaApplicable: true,
+      },
+      {
+        id: 2,
+        chrono: "D-2021-000002",
+        status: "NEW",
+        customer: {
+          id: 1,
+          firstname: "Foo",
+          lastname: "Bar",
+        },
+        sentAt: "2021-03-26T11:48:18+00:00",
+        paidAt: null,
+        paymentDeadline: "2021-03-26T11:48:18+00:00",
+        validityDate: "2021-03-26T11:48:18+00:00",
+        workStartDate: "2021-03-26T11:48:18+00:00",
+        workDuration: "2 weeks",
+        paymentDelayRate: 0,
+        services: [{ name: "", quantity: 1, unitPrice: 0 }],
+        tvaApplicable: true,
+      },
+    ],
+  })
+);
+
+describe("Devis Page", () => {
+  beforeEach(async () => {
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <Devis />
+        </MemoryRouter>
+      );
+    });
+  });
+
+  it("should display the page title", () => {
+    expect(document.title).toBe("Mes devis - Microgest");
+  });
+
+  it("should render the Devis in the table", () => {
+    expect(screen.getByText("D-2021-000001").tagName).toBe("A");
+    expect(screen.getByText("D-2021-000002").tagName).toBe("A");
+  });
+});
